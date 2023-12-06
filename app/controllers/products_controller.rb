e3bb6c78bac3d3b5771d0209e817168e28ec1bfb
Eunice <117746227+Eueu26@users.noreply.github.com>
2023-12-05 22:07:02 -0500

    make users session more private with authentication

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,11 +1,18 @@
-class ProductsController < ApplicationController
 
+class Admin::ProductsController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USERNAME'], password: ENV['ADMIN_PASSWORD'], except: :index
+  
+end
+
+
+class ProductsController < ApplicationController
+  
   def index
     @products = Product.all.order(created_at: :desc)
   end
-
+  
   def show
     @product = Product.find params[:id]
   end
-
+  
 end
